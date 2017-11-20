0272c5f71fd1ab9a2e446e2990c5857a42f832e2
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-20 23:36:44 +0100

    Renamed database_test and created a sqlite script
    Hopefully, the script will be able to handle most kinds of input data and store it into a database. The two major functions are adding into database and finding in database. I need to find a way to remove duplicates with the same key. All is based on the user having an associated key that they will use for accessing the database. The database is not meant to store results indefinetly, just for shorter periods of time. However, if I can remove and clear junk from the database, it can be used permanently. NOTE: Might be susceptible for SQL injections. Needs to be fixed

--- scripts/databases_test.py
+++
@@ -1,4 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("emails.db")
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE email_addresses ( email TEXT );")
---
+++ scripts/sql_handler.py
@@ -0,0 +1,44 @@
+import sqlite3
+import re
+
+class sql_handler():
+
+    def __init__(self):
+        self.delimiter = r'␞'
+    
+    def dict_to_db(self, value_dict):
+        """Makes list in dict into a delimited string"""
+        for i in value_dict:
+            if type(value_dict[i]) is list:
+                value_dict[i] = self.delimiter.join([str(x) for x in value_dict[i]]) # Join by ␞
+        return value_dict
+
+    def db_to_list(self, db_result):
+        """Converts tuple from SQL query into a list"""
+        db_result = list(db_result)
+        for i,j in enumerate(db_result):
+            if bool(re.search(self.delimiter, str(db_result[i]))):
+                db_result[i] = j.split(self.delimiter)
+        return db_result
+
+    def db_add(self, table_name, value_dict):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        value_dict = self.dict_to_db(value_dict)
+        # RISK FOR SQL INJECTION, BEWARE
+        c.execute('''CREATE TABLE IF NOT EXISTS {} {}'''.format(table_name,
+                                                                str(tuple(value_dict))))
+        # This is just stupid....
+        c.execute("INSERT INTO {} VALUES {}".format(table_name,
+                                                    str(tuple(value_dict.values()))))
+        conn.commit()
+        conn.close()
+
+    def db_find(self, table_name, key):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM {} WHERE KEY="{}"'.format(table_name, key))
+        db_result = self.db_to_list(c.fetchone())
+        conn.close()
+        del db_result[-1] # Remove key
+        return db_result

57e80f50bba91ea1ae2397782f37fad7412c6ff7
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2017-11-22 00:31:56 +0100

    Class based method testing
    Testing to implement a class based method for a page

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ from flask import send_from_directory, send_file  # Downloading and uploading
 from werkzeug.utils import secure_filename  # Uploading
 from flask import flash  # Alerts and flashing
 from flask import Flask, render_template  # Basic rendering
-from flask import Response # Stream
+from flask.views import View
+from flask.views import MethodView
 from flask import jsonify
+from flask import Response # Stream
 import json
 import threading
 import subprocess
@@ -37,7 +39,6 @@ ALLOWED_EXTENSIONS = set(['txt', 'fasta', 'fa', 'sthlm'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['JSON_AS_ASCII'] = False
 
-
 # Root page
 @app.route('/')
 def index():
@@ -55,41 +56,139 @@ def index():
 def about():
     return render_template('about.html')
 
+class PairwiseClass(View):
+    page = 'Pairwise'
+    folder = 'Alignment'
+    methods = ['POST','GET']
+
+    def __init__(self):
+        pass
+
+    def dispatch_request(self):
+        if request.path == '/Pairwise':
+            return self.Pairwise()
+        elif request.path == '/Pairwise/post':
+            return self.post()
+        elif request.path == '/Pairwise/get':
+            return self.get()
+        else:
+            abort(404)
+
+    def Pairwise(self):
+        finished = False
+        if session.get(self.page+'_key') == None:
+            session[self.page+'_key'] = key_generator()
+        elif session.get(self.page+'_done') != None:
+            finished = True
+        return render_template('/'+self.folder+'/'+self.page+'.html',
+                               finished=finished,
+                               page=self.page)
+
+    def post(self):
+        if request.form['fasta_seq'] == '':
+            flash('No file or input detected')
+            return redirect(url_for(self.page))
+        elif ('file' in request.files) and (request.files['file'].filename != ''):
+            file = request.files['file']  #Process file
+            session['filename'] = file.filename # add filename to session
+            if not allowed_file(file.filename):
+                flash('File type not supported')
+                return redirect(url_for(self.page))
+            file_contents = file.read().decode()  #If everything is ok, read
+            from scripts.read_fasta import read_fasta
+            names, sequences = read_fasta(file_contents)
+        else:
+            fasta_seq = request.form['fasta_seq']
+            from scripts.read_fasta import read_fasta
+            names, sequences = read_fasta(fasta_seq)
+        if len(sequences) < 2:
+            flash('Only one sequence detected')
+            return redirect(url_for(self.page))
+        from scripts.Alignment.NW import Needleman_Wunsch
+        queue = multiprocessing.Queue()
+        align_process = multiprocessing.Process(target=Needleman_Wunsch().run,
+                                                args=[sequences[0], sequences[1], queue])
+        align_process.start()
+        data_dict = queue.get() # ADD ASYNCHRONOUS WAIT
+        data_dict['KEY'] = session[self.page + '_key']  # Add key for query
+        from scripts.sql_handler import sql_handler
+        sql_handler().db_clear(self.page, session[self.page + '_key'])  # Clear previous entries
+        sql_handler().db_add(self.page, data_dict)
+        session[self.page + '_done'] = 'Done'
+        return redirect(url_for(self.page))
+
+    def get(self):
+        """
+        Problem fixed. If you use request.form['view'] first while clicking on download,
+        the app will crash since it tries to find it in the if statement
+        """
+        if request.form.get('reset') != None:
+            session.clear()
+            return redirect(url_for(self.page))
+        elif request.form.get('download') != None:
+            bIO = io.BytesIO()
+            from scripts.sql_handler import sql_handler
+            from scripts.fix_list import fix_list
+            bIO.write(fix_list(sql_handler().db_find(self.page,
+                               session[self.page+'_key']))
+                               .encode())
+            bIO.seek(0)
+            if session.get('filename') != None:
+                filename = 'aligned_' + session['filename']
+            else:
+                filename = 'aligned_sequences.fasta'
+            return send_file(bIO,
+                            attachment_filename=filename,
+                            as_attachment=True)
+        elif request.form.get('view') != None:
+            from scripts.sql_handler import sql_handler
+            from scripts.fix_list import fix_list
+            # OBS: <tt> tag is not supported in HTML5
+            return '<tt>'+fix_list(sql_handler().db_find(self.page,
+                            session[self.page+'_key']),
+                            html=True)
+app.add_url_rule('/Pairwise', view_func=PairwiseClass.as_view('Pairwise'))
+app.add_url_rule('/Pairwise/post', view_func=PairwiseClass.as_view('Pairwise_post'))
+app.add_url_rule('/Pairwise/get', view_func=PairwiseClass.as_view('Pairwise_get'))
+
+
 # MSA ROOT
 @app.route('/MSA')
 def MSA():
-    page = 'MSA'; folder = 'Alignment'
+    page = 'MSA';
+    folder = 'Alignment'
     if session.get('processed_MSA') != None:
         flag = True
     else:
         flag = False
-    return render_template('/'+folder+'/'+page+'.html',
+    return render_template('/' + folder + '/' + page + '.html',
                            processed=flag, page=page)
 
 # MSA POST
 @app.route('/MSA/post', methods=['POST'])
 def MSA_get_fasta():
-    page = 'MSA'; folder = 'Alignment'
+    page = 'MSA';
+    folder = 'Alignment'
     if request.form['fasta_seq'] == '':
         flash('No file or input detected')
         return redirect(url_for(page))
 
-    #Prioritize files. Check if file exist
+    # Prioritize files. Check if file exist
     elif ('file' in request.files) and (request.files['file'].filename != ''):
 
-        file = request.files['file']  #Process file
-        session['filename'] = file.filename #add filename to session
+        file = request.files['file']  # Process file
+        session['filename'] = file.filename  # add filename to session
 
-        #Check if the file extension is allowed
+        # Check if the file extension is allowed
         if not allowed_file(file.filename):
             flash('File type not supported')
             return redirect(url_for(page))
 
-        file_contents = file.read()  #If everything is ok, read
-        #Remove all comments etc
+        file_contents = file.read()  # If everything is ok, read
+        # Remove all comments etc
         processed_MSA = file_contents
 
-    #Text field
+    # Text field
     else:
         fasta_seq = request.form['fasta_seq']
         processed_MSA = fasta_seq.upper()
@@ -98,7 +197,7 @@ def MSA_get_fasta():
 
     return redirect(url_for(page))
 
-#MSA GET
+# MSA GET
 @app.route('/MSA/get', methods=['POST'])
 def MSA_send_fasta():
     page = 'MSA'
@@ -128,97 +227,10 @@ def MSA_send_fasta():
             filename = 'aligned_sequences.fasta'
 
         return send_file(bIO,
-                        attachment_filename=filename,
-                        as_attachment=True)
+                         attachment_filename=filename,
+                         as_attachment=True)
     elif request.form.get('view') != None:
         return session['processed_MSA']
-
-#Pairwise ROOT
-@app.route('/Pairwise')
-def Pairwise():
-    page = 'Pairwise'; folder = 'Alignment'
-    finished = False
-    if session.get(page+'_key') == None:
-        session[page+'_key'] = key_generator()
-    elif session.get(page+'_done') != None:
-        finished = True
-    else:
-        print('Error for user' + session[page+'_key'])
-    return render_template('/'+folder+'/'+page+'.html',
-                           finished=finished,
-                           page=page)
-
-#Pairwise POST
-@app.route('/Pairwise/post', methods=['POST'])
-def Pairwise_get_fasta():
-    page = 'Pairwise'; folder = 'Alignment'
-    if request.form['fasta_seq'] == '':
-        flash('No file or input detected')
-        return redirect(url_for(page))
-    elif ('file' in request.files) and (request.files['file'].filename != ''):
-        file = request.files['file']  #Process file
-        session['filename'] = file.filename # add filename to session
-        if not allowed_file(file.filename):
-            flash('File type not supported')
-            return redirect(url_for(page))
-        file_contents = file.read().decode()  #If everything is ok, read
-        from scripts.read_fasta import read_fasta
-        names, sequences = read_fasta(file_contents)
-    else:
-        fasta_seq = request.form['fasta_seq']
-        from scripts.read_fasta import read_fasta
-        names, sequences = read_fasta(fasta_seq)
-    if len(sequences) < 2:
-        flash('Only one sequence detected')
-        return redirect(url_for(page))
-    from scripts.Alignment.NW import Needleman_Wunsch
-    queue = multiprocessing.Queue()
-    align_process = multiprocessing.Process(target=Needleman_Wunsch().run,
-                                            args=[sequences[0], sequences[1], queue])
-    align_process.start()
-    data_dict = queue.get()
-    data_dict['KEY'] = session[page+'_key']  # Add key for query
-    from scripts.sql_handler import sql_handler
-    sql_handler().db_clear(page, session[page+'_key'])  # Clear previous entries
-    database_process = multiprocessing.Process(target=sql_handler().db_add,
-                                               args=[page, data_dict])
-    database_process.start()
-    session[page+'_done'] = 'Done'
-    return redirect(url_for(page))
-
-#Pairwise GET
-@app.route('/Pairwise/get', methods=['POST'])
-def Pairwise_send_fasta():
-    page = 'Pairwise'; folder = 'Alignment'
-    """
-    Problem fixed. If you use request.form['view'] first while clicking on download,
-    the app will crash since it tries to find it in the if statement
-    """
-    if request.form.get('reset') != None:
-        session.clear()
-        return redirect(url_for(page))
-    elif request.form.get('download') != None:
-        bIO = io.BytesIO()
-        from scripts.sql_handler import sql_handler
-        from scripts.fix_list import fix_list
-        bIO.write(fix_list(sql_handler().db_find(page,
-                           session[page+'_key']))
-                           .encode())
-        bIO.seek(0)
-        if session.get('filename') != None:
-            filename = 'aligned_' + session['filename']
-        else:
-            filename = 'aligned_sequences.fasta'
-        return send_file(bIO,
-                        attachment_filename=filename,
-                        as_attachment=True)
-    elif request.form.get('view') != None:
-        from scripts.sql_handler import sql_handler
-        from scripts.fix_list import fix_list
-        # OBS: <tt> tag is not supported in HTML5
-        return '<tt>'+fix_list(sql_handler().db_find(page,
-                        session[page+'_key']),
-                        html=True)
 
 #Random DNA page
 @app.route('/Random_DNA')
@@ -246,7 +258,14 @@ if __name__ == "__main__":
 
 
 
-"""https://stackoverflow.com/questions/14672753/handling-multiple-requests-in-flask"""
+"""
+https://stackoverflow.com/questions/14672753/handling-multiple-requests-in-flask
+https://stackoverflow.com/questions/8179558/how-to-pass-classs-self-through-a-flask-blueprint-route-decorator
+http://flask.pocoo.org/docs/0.12/blueprints/
+https://www.reddit.com/r/flask/comments/3xk4dq/class_based_views_or_function_based_views/
+https://stackoverflow.com/questions/27192932/define-manually-routes-using-flask
+"""
+
 
 ## DUMP
 
